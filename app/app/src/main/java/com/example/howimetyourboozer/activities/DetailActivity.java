42a62ca42e17be17bef1bcfc555e70c72f691f5e
Léo <leodehondt@msn.com>
2021-02-15 17:27:05 +0100

    Creation activitydetails

---
+++ app/app/src/main/java/com/example/howimetyourboozer/activities/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.howimetyourboozer.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.howimetyourboozer.R;
+
+public class DetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
